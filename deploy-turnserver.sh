b3e34de7f49874ad8aea24727cf84c465a0ec5fb
David Dvorak <dvorak.dave@gmail.com>
2018-07-16 12:58:56 +0200

    Port + debug setup

--- deploy-turnserver.sh
+++ deploy-turnserver.sh
@@ -5,7 +5,7 @@ echo $3
 internalIp="$(ip a | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
 externalIp="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
-echo "listening-port=3478
+echo "listening-port=80
 tls-listening-port=5349
 listening-ip="$internalIp"
 relay-ip="$internalIp"
@@ -17,8 +17,11 @@ userdb=/var/lib/turn/turndb
 # use real-valid certificate/privatekey files
 cert=/etc/ssl/turn_server_cert.pem
 pkey=/etc/ssl/turn_server_pkey.pem
- 
-no-stdout-log"  | tee /etc/turnserver.conf
+min-port=25000
+max-port=30000
+log-file=stdout
+verbose
+"  | tee /etc/turnserver.conf
 
 
 turnadmin -a -u $1 -p $2 -r $3
